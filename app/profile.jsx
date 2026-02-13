e8df30a5628ad7f61d76d3ca63d7c2dd571c8220
Faiyyaz25003 <khanwork25003@gmail.com>
2026-02-14 02:04:00 +0530

    refactor: remove old ChangePassword component and implement new one

--- app/profile.jsx
+++ app/profile.jsx
@@ -112,6 +112,7 @@ export default function ProfileScreen() {
                 // navigation.navigate('ChangePassword')
 
                 router.push("/changePassword");
+
               }}
             >
               <View
